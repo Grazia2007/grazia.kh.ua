dc6be22256d4549c5283154c88cbfb6d0a5c5a39
Grazia2007 <mebelgrz@gmail.com>
2026-06-05 12:23:36 +0000

    add: database and admin protection v1.1

--- src/app/blablabladmin/page.tsx
+++ src/app/blablabladmin/page.tsx
@@ -144,7 +144,7 @@ export default function AdminPanel() {
     if (!window.confirm('Ви впевнені, що хочете назавжди видалити цей проєкт?')) return;
     try {
       setStatusMessage({ type: 'info', text: 'Видалення...' });
-      const res = await fetch(`/api/admin/projects?id=${id}`, {
+      const res = await fetch(`/api/blablabladmin/projects?id=${id}`, {
         method: 'DELETE',
         headers: { 'Authorization': `Bearer ${passwordInput}` } // Передаємо пароль для перевірки!
       });
@@ -450,8 +450,8 @@ const uploadPhotos = async () => {
 
     const method = editingId ? 'PATCH' : 'POST';
       const endpoint = editingId 
-        ? `/api/admin/projects?id=${editingId}`
-        : `/api/admin/projects`;
+        ? `/api/blablabladmin/projects?id=${editingId}`
+        : `/api/blablabladmin/projects`;
 
       const dbRes = await fetch(endpoint, {
         method: method,
